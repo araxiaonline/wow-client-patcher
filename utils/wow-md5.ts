974ecf1722b4e032c9502104bfa2ccf513b519a9
Ben <ben.carter.dev@gmail.com>
2023-10-02 19:49:40 -0500

    md5 function

---
+++ utils/wow-md5.ts
@@ -0,0 +1,16 @@
+// import * as fs from 'fs';
+// import * as crypto from 'crypto';
+// import path from 'path';
+
+// const filePath = path.resolve('extraResources/Wow.exe'); // Replace with the path to your file
+// const fileStream = fs.createReadStream(filePath);
+// const hash = crypto.createHash('md5');
+
+// fileStream.on('data', (data) => {
+//   hash.update(data);
+// });
+
+// fileStream.on('end', () => {
+//   const md5Hash = hash.digest('hex');
+//   console.log(`MD5 hash of the file: ${md5Hash}`);
+// });
