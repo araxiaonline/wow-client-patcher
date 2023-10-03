85b7bf6778d652137e8f374029fba23c1cb447bc
Ben <ben.carter.dev@gmail.com>
2023-10-02 19:47:54 -0500

    Added constants use for methods and api

---
+++ src/constants/index.ts
@@ -0,0 +1,24 @@
+const Channels = {
+  WOW_LAUNCH: 'launch-wow',
+  WOW_LAUNCH_ERROR: 'launch-wow-error',
+  WOW_CLIENT_EXIT: 'launch-wow-exit',
+  APP_INFO: 'app-info', // Initial application information
+  APP_API: 'api-invoke',
+  DOWNLOAD_START: 'download-start',
+  DOWNLOAD_END: 'download-end',
+  DOWNLOAD_PROGRESS: 'download-progress',
+  DOWNLOAD_BATCH_START: 'download-batch-start',
+  DOWNLOAD_BATCH_END: 'download-batch-end',
+  DOWNLOAD_BATCH_DATA: 'download-batch-data',
+} as const;
+
+const PLATFORM = {
+  WINDOWS: 'win32',
+  MAC: 'darwin',
+  LINUX: 'linux',
+} as const
+
+export{
+  Channels,
+  PLATFORM,
+}
