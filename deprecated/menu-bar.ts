dee256f56ef62a17d2d442747bf7c26cd05071d9
Ben <ben.carter.dev@gmail.com>
2024-03-01 18:51:31 -0500

    updated version

---
+++ deprecated/menu-bar.ts
@@ -0,0 +1,186 @@
+// {
+//   /* Music & Optional patches component */
+// }
+// {
+//   /* <Paper
+//                 onClick={installMisc}
+//                 elevation={6}
+//                 sx={{
+//                   padding: '3px',
+//                   height: '65px',
+//                   transition: !appInfo?.Misc ? 'all 0.3s ease-out' : 'none',
+//                   ':hover': !appInfo?.Misc
+//                     ? {
+//                         cursor: 'pointer',
+//                         backgroundColor: 'rgba(235,0,0,0.7)',
+//                         transition: 'all 0.3s ease-out',
+//                       }
+//                     : 'none',
+//                   backgroundColor: appInfo?.Misc
+//                     ? 'rgba(30,30,40,0.8)'
+//                     : 'rgba(250,0,19,0.5)',
+//                   border: '1px solid rgba(254,223,164,0.6)',
+//                 }}
+//               > */
+// }
+// {
+//   /* <Grid
+//                   container
+//                   direction="row"
+//                   height="100%"
+//                   alignItems="center"
+//                   padding={1}
+//                 >
+//                   <Grid xs={3}>
+//                     <img src={musicIcon} alt="wow" width="50px" />
+//                   </Grid>
+//                   <Grid xs={9} padding={1}>
+//                     <Typography variant="body2" sx={{ color: 'white' }}>
+//                       {appInfo?.Misc
+//                         ? 'Music & Optional patches installed'
+//                         : 'Install HD Music & Login Screen '}
+//                     </Typography>
+//                   </Grid>
+//                 </Grid> */
+// }
+// {
+//   /* WoW Client: {appInfo?.WoWPatched ? <span className="success">Patched</span> : <span className="error">Not Patched</span>} */
+// }
+// {
+//   /* </Paper> */
+// }
+
+
+
+              // {/* HD patches component */}
+              // <Paper
+              //   onClick={installHD}
+              //   elevation={6}
+              //   sx={{
+              //     padding: '3px',
+              //     height: '65px',
+              //     transition: !appInfo?.HD ? 'all 0.3s ease-out' : 'none',
+              //     ':hover': !appInfo?.HD
+              //       ? {
+              //           cursor: 'pointer',
+              //           backgroundColor: 'rgba(235,0,0,0.7)',
+              //           transition: 'all 0.3s ease-out',
+              //         }
+              //       : 'none',
+              //     backgroundColor: appInfo?.HD
+              //       ? 'rgba(30,30,40,0.8)'
+              //       : 'rgba(250,0,19,0.5)',
+              //     border: '1px solid rgba(254,223,164,0.6)',
+              //   }}
+              // >
+              //   <Grid
+              //     container
+              //     direction="row"
+              //     height="100%"
+              //     alignItems="center"
+              //     padding={1}
+              //   >
+              //     <Grid xs={3}>
+              //       <img src={hdIcon} alt="wow" width="50px" />
+              //     </Grid>
+              //     <Grid xs={9} padding={1}>
+              //       <Typography variant="body2" sx={{ color: 'white' }}>
+              //         {appInfo?.HD
+              //           ? 'HD Client Installed'
+              //           : 'Install HD Client'}
+              //       </Typography>
+              //     </Grid>
+              //   </Grid>
+              // </Paper>
+
+
+
+                          //   {/* Store menu component */}
+
+                          //   <Paper
+                          //   onClick={installStore}
+                          //   elevation={6}
+                          //   sx={{
+                          //     padding: '3px',
+                          //     height: '65px',
+                          //     transition: !appInfo?.AIOInstalled
+                          //       ? 'all 0.3s ease-out'
+                          //       : 'none',
+                          //     ':hover': !appInfo?.AIOInstalled
+                          //       ? {
+                          //           cursor: 'pointer',
+                          //           backgroundColor: 'rgba(235,0,0,0.7)',
+                          //           transition: 'all 0.3s ease-out',
+                          //         }
+                          //       : 'none',
+                          //     backgroundColor: appInfo?.AIOInstalled
+                          //       ? 'rgba(30,30,40,0.8)'
+                          //       : 'rgba(250,0,19,0.5)',
+                          //     border: '1px solid rgba(254,223,164,0.6)',
+                          //   }}
+                          // >
+                          //   <Grid
+                          //     container
+                          //     direction="row"
+                          //     height="100%"
+                          //     alignItems="center"
+                          //     padding={1}
+                          //   >
+                          //     <Grid xs={3}>
+                          //       <img src={moneyIcon} alt="wow" width="50px" />
+                          //     </Grid>
+                          //     <Grid xs={9} padding={1}>
+                          //       <Typography variant="body2" sx={{ color: 'white' }}>
+                          //         {appInfo?.AIOInstalled
+                          //           ? 'Araxia Store Installed'
+                          //           : 'Install Araxia Store'}
+                          //       </Typography>
+                          //     </Grid>
+                          //   </Grid>
+                          // </Paper> */}
+
+
+                                      {/* <Grid xs={4}>
+              <Paper
+                onClick={installWow}
+                elevation={6}
+                sx={{
+                  padding: '3px',
+                  height: '65px',
+                  transition: !appInfo?.WoWPatched
+                    ? 'all 0.3s ease-out'
+                    : 'none',
+                  ':hover': !appInfo?.WoWPatched
+                    ? {
+                        cursor: 'pointer',
+                        backgroundColor: 'rgba(235,0,0,0.7)',
+                        transition: 'all 0.3s ease-out',
+                      }
+                    : 'none',
+                  backgroundColor: appInfo?.WoWPatched
+                    ? 'rgba(30,30,40,0.8)'
+                    : 'rgba(250,0,19,0.5)',
+                  border: '1px solid rgba(254,223,164,0.6)',
+                }}
+              >
+                <Grid
+                  container
+                  direction="row"
+                  height="100%"
+                  alignItems="center"
+                  padding={1}
+                >
+                  <Grid xs={3}>
+                    <img src={wowInstalled} alt="wow" width="49px" />
+                  </Grid>
+                  <Grid xs={9} padding={1}>
+                    <Typography variant="body2" sx={{ color: 'white' }}>
+                      {appInfo?.WoWPatched
+                        ? 'Wow.exe is optimized '
+                        : 'Install patched Wow.exe'}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Paper>
+
+            </Grid> */}
